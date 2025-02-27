8e6095c93edb4feca8bf9dbf3e3035dc2cba9ddd
Muhammad Adil Mehmood <iamadilmalick@gmail.com>
2025-02-27 21:21:39 +0500

    adding quizler with logic to check answer with 2d array and loop instead of ending at any certain level

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    
+    let questions = [
+        ["Four + Two = Six?", "True"],
+        ["10 - 5 = 3", "True"],
+        ["Lion is an animal", "True"],
+        ["Cat have 2 legs & 2 arms", "False"],
+    ]
+    var currentQuestionIndex : Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        renderQuestion()
     }
-
+    
+    
+    @IBAction func onButtonPressed(_ sender: UIButton) {
+        let userAnswer : String = sender.currentTitle!
+        let actualAnswer : String = questions[currentQuestionIndex][1]
+        
+        if userAnswer == actualAnswer {
+            print("Correct!")
+        } else {
+            print("Incorrect!")
+        }
+        
+        if currentQuestionIndex == questions.count - 1 {
+            currentQuestionIndex = 0
+        }else {
+            currentQuestionIndex += 1
+        }
+        
+        renderQuestion()
+    }
+    
+    func renderQuestion() {
+        questionLabel.text = questions[currentQuestionIndex][0]
+    }
+    
 
 }
 
